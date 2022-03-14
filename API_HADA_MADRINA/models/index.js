1ae4506f4403115375a7d273ba371e5717480483
Aranzazu0611 <huella85@gmail.com>
2022-03-14 12:34:59 +0100

    refactor to ternary

--- API_HADA_MADRINA/models/index.js
+++ API_HADA_MADRINA/models/index.js
@@ -1,8 +1,7 @@
 const operation_Database = (connection, query, operation,  name_database) =>{
   
   connection.query(query, (error) => {
-      if(error)  console.error('error: ' + error.message);          
-      console.log( operation + name_database);
+      (error) ? console.error('error: ' + error.message): console.log( operation + name_database);     
     
   });
 };
