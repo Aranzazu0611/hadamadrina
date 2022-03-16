1259956761a409fcf0d43949fe00454b092c9b2a
Aranzazu0611 <huella85@gmail.com>
2022-03-16 21:43:46 +0100

    add create_database file

--- API_HADA_MADRINA/models/index.js
+++ API_HADA_MADRINA/models/index.js
@@ -2,9 +2,11 @@ const operation_Database = (connection, query, operation,  name_database) =>{
   
   connection.query(query, (error) => {
       (error) ? console.error('error: ' + error.message): console.log( operation + name_database);    
-    
+      
   });
+  
 };
+
 
 module.exports = { 
   operation_Database
